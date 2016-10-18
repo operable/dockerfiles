1b0c0ab7a976f3ccd6db5e1d74921ffab52babc0
Christopher Maier <chris@operable.io>
2016-10-18 15:59:20 -0400

    Add a Go Dockerfile

---
+++ Dockerfile.go
@@ -0,0 +1,11 @@
+FROM operable/alpine-base:0.1
+
+ENV GO_PACKAGE_VERSION 1.6.3-r0
+ENV GOPATH /gopath
+
+RUN apk -U add \
+    go=$GO_PACKAGE_VERSION \
+    go-tools=$GO_PACKAGE_VERSION
+
+RUN go get -u github.com/kardianos/govendor
+RUN go get -u github.com/golang/lint/golint
